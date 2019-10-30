eea677d3522b82f0d9a196142fcb79f2eeffd10e
Haoyang Ren <ryanreninau@gmail.com>
2019-10-31 01:10:46 +1100

    Set up the prototype of Kanban class

---
+++ Knowledge-Worker-Clarity/src/Kanban.java
@@ -0,0 +1,73 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ryan
+ */
+public class Kanban {
+    private LocalDate start;
+    private LocalDate end;
+    private double duration;
+    private String description;
+    private String category;
+
+    public Kanban(LocalDate start, LocalDate end, double duration, String description, String category) {
+        this.start = start;
+        this.end = end;
+        this.duration = duration;
+        this.description = description;
+        this.category = category;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    
+    
+    
+}
+
