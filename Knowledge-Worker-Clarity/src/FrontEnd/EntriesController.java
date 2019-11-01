e6c7fc4e84a02b223d5bfa95291031941b9cc886
Haoyang Ren <ryanreninau@gmail.com>
2019-11-01 17:35:45 +1100

    Create Entry Screen class

--- Knowledge-Worker-Clarity/src/FrontEnd/EntriesController.java
+++ Knowledge-Worker-Clarity/src/FrontEnd/EntriesController.java
@@ -12,14 +12,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-
 /**
  * FXML Controller class
  *
  * @author ryan
  */
-
-
 public class EntriesController {
 
     @FXML
@@ -51,20 +48,57 @@ public class EntriesController {
 
     @FXML
     private Button confirm;
-    
+
     Database d = new Database();
     //TODO: Instatiate the PageSwitchHelper class
     PageSwitcher pageSwitcher = new PageSwitcher();
-    
+
     @FXML
     private void handleLoginButtonAction(ActionEvent event) {
-        
+        /*String userString = username.getText();
+        String passString = password.getText();
+        String getResult = "SELECT * FROM USER "
+                + "WHERE username = '" + userString + "' "
+                + "AND password = '" + passString + "';";
+
+        try {
+            ResultSet rs = d.getResultSet(getResult); //TODO: Fill in this query
+            if (!rs.next()) {
+                //TODO: What should happen if there is no result?
+                System.out.println("Login Failed!");
+            } else {
+                //TODO: What should happen if there is a result?
+                System.out.println("Login Success!");
+                psh.switcher(event, "MusicList.fxml");
+            }
+            rs.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }*/
+
+        String startText = startTime.getText();
+        String endText = endTime.getText();
+        String descriptionText = description.getText();
+        String taskTitleText = taskTitle.getText();
+        String taskDescriptionText = taskDescription.getText();
+        String doDateText = doDate.getText();
+        String dueDateText = dueDate.getText();
+        String priorityText = priority.getText();
+        String selectedCategory = category.getAccessibleText();
+
+        try {
+            
+            pageSwitcher.switcher(event, "Entries.fxml");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 
     @FXML
     public void initialize() {
         //TODO: What should the screen look like when it loads?
-        
+
     }
 
 }
---
+++ Knowledge-Worker-Clarity/src/FrontEnd/EntryScreen.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FrontEnd;
+
+import java.sql.SQLException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ryan
+ */
+public class EntryScreen extends Application { //TODO: What class do you need to extend?
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //loadDatabase();
+        Parent root = FXMLLoader.load(getClass().getResource("Entries.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void loadDatabase() throws SQLException {
+       //TODO: Call Database methods here to create the database
+
+       
+    }
+    
+}
