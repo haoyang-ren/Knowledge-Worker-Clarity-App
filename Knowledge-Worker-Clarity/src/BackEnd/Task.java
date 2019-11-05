b61e0c5cadc25b3017b4c908d7951b1baf2237a6
Haoyang Ren <ryanreninau@gmail.com>
2019-11-05 12:45:44 +1100

    Create the task class

--- Knowledge-Worker-Clarity/src/BackEnd/Task.java
+++ Knowledge-Worker-Clarity/src/BackEnd/Task.java
@@ -12,9 +12,60 @@ import java.time.LocalDate;
  * @author ryan
  */
 public class Task {
-    private String title;
-    private String description;
-    private LocalDate doDate;
-    private LocalDate dueDate;
-    private int priority;
+    private String taskTitle;
+    private String taskDescription;
+    private String doDate;
+    private String dueDate;
+    private String priority;
+
+    public Task(String taskTitle, String taskDescription, String doDate, String dueDate, String priority) {
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+        this.doDate = doDate;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getDoDate() {
+        return doDate;
+    }
+
+    public void setDoDate(String doDate) {
+        this.doDate = doDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+    
+    
+    
 }
