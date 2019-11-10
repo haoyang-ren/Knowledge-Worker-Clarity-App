202b240c388b75f7aaf943530580e275abad181a
Luke Strong <lukeglenstrong@gmail.com>
2019-11-11 10:38:39 +1100

    Deep Focus Screen
    Basic layout of deep focus screen

--- Knowledge-Worker-Clarity/src/FrontEnd/DeepFocusScreenController.java
+++ Knowledge-Worker-Clarity/src/FrontEnd/DeepFocusScreenController.java
@@ -6,15 +6,24 @@
 package FrontEnd;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import javafx.event.ActionEvent;
 
 /**
  * FXML Controller class
  *
- * @author calvinkoder
+ * @author lukestrong
  */
 public class DeepFocusScreenController implements Initializable {
 
@@ -23,9 +32,35 @@ public class DeepFocusScreenController implements Initializable {
     @FXML
     private Label DeepFocusLabel;
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    private TextField timefield;
     
+    @FXML
+    private TextField datefield;
+    
+    @FXML
+    private TextField tasknametext;
+    
+    @FXML
+    private TextField taskdesctext;
+    
+
+    
+    
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    
+    Date date = new Date();
+    
+    
+    //datefield.setText(dateFormat.format(date));
+       @FXML
+        public void initialize() {
+         try {
+               timefield.setText("Hi");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
