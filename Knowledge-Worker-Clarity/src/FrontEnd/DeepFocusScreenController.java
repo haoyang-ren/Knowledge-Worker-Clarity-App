2c306b059af7d548f82f3eb2f7cda0aa8a10fbbc
Calvin Koder <calvin.koder@yahoo.com>
2019-11-10 14:14:14 +1100

    Added deepfocus fxml and controller

---
+++ Knowledge-Worker-Clarity/src/FrontEnd/DeepFocusScreenController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FrontEnd;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author calvinkoder
+ */
+public class DeepFocusScreenController implements Initializable {
+
+    
+    
+    @FXML
+    private Label DeepFocusLabel;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
