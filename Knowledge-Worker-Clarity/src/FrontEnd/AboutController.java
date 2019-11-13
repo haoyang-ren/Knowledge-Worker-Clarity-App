25eacaa7207ba99af44c290ba23b7eca4e530b35
Goochlord <56661603+Goochlord@users.noreply.github.com>
2019-11-13 17:39:37 +1100

    Filled in about screen

--- Knowledge-Worker-Clarity/src/FrontEnd/AboutController.java
+++ Knowledge-Worker-Clarity/src/FrontEnd/AboutController.java
@@ -5,9 +5,14 @@
  */
 package FrontEnd;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -19,9 +24,23 @@ public class AboutController implements Initializable {
     /**
      * Initializes the controller class.
      */
+
+    @FXML
+    private Button back;
+
+   
+    PageSwitcher pageSwitcher = new PageSwitcher();
+
+    @FXML
+    void handleBackButtonAction(ActionEvent event) throws IOException {
+    pageSwitcher.switcher(event, "Dashboard.fxml");    
+        
+        
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
     
 }
